06e7db6afc4f93a01e8262e168830dc4bcd4d025
Hans Meyer <hamster.of.dev@gmail.com>
2015-04-28 21:45:20 +0200

    :postal_horn: Locale aware output
    As users are likely to understand things better in their own language

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
-#include <elmo.hpp>
 #include <iostream>
+#include <locale>
+
+void make_locale_aware() {
+    std::locale::global(std::locale(""));
+    std::cout.imbue(std::locale(""));
+}
 
 int main() {
+    make_locale_aware();
     std::cout << "Hello world" << std::endl;
 }
+
